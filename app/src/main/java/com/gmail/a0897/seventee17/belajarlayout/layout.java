8b4ba9de48cea46270df570322ef3208fe7b8de6
MuhammadBilalR <46312294+MuhammadBilalR@users.noreply.github.com>
2019-05-07 11:35:25 +0700

    Initial commit

---
+++ app/src/main/java/com/gmail/a0897/seventee17/belajarlayout/layout.java
@@ -0,0 +1,15 @@
+package com.gmail.a0897.seventee17.belajarlayout;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by student on 3/26/2019.
+ */
+public class layout extends Activity {
+
+}
